13c4d1dcf279a9e7ac6e4a6aeaf2b1784f92f0c7
Yayo <geramail28@gmail.com>
2021-02-16 15:52:14 +0800

    Rename client to provider.

--- test/news_provider_test.dart
+++ test/news_provider_test.dart
@@ -7,8 +7,8 @@ import 'package:http_unit_test/src/data_provider/news_provider.dart';
 
 void main() {
   test('Top headlines response is correct', () async {
-    final client = _getClient('test/top_headlines.json');
-    final articles = await client.topHeadlines('us');
+    final provider = _getProvider('test/top_headlines.json');
+    final articles = await provider.topHeadlines('us');
 
     expect(articles.length, 2);
     expect(articles[0].author, 'Sophie Lewis');
@@ -16,19 +16,19 @@ void main() {
   });
 
   test('Api key missing exception is thrown correctly', () async {
-    final client = _getClient('test/api_key_missing.json');
-    expect(client.topHeadlines('mx'), throwsA(predicate((exception) => exception is MissingApiKeyException)));
+    final provider = _getProvider('test/api_key_missing.json');
+    expect(provider.topHeadlines('mx'), throwsA(predicate((exception) => exception is MissingApiKeyException)));
   });
 
   test('Invalid Api key exception is thrown correctly', () async {
-    final client = _getClient('test/api_key_invalid.json');
-    expect(client.topHeadlines('mx'), throwsA(predicate((exception) => exception is ApiKeyInvalidException)));
+    final provider = _getProvider('test/api_key_invalid.json');
+    expect(provider.topHeadlines('mx'), throwsA(predicate((exception) => exception is ApiKeyInvalidException)));
   });
 }
 
 final headers = {HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'};
 
-NewsProvider _getClient(String filePath) => NewsProvider(httpClient: _getMockClient(filePath));
+NewsProvider _getProvider(String filePath) => NewsProvider(httpClient: _getMockProvider(filePath));
 
-MockClient _getMockClient(String filePath) =>
+MockClient _getMockProvider(String filePath) =>
     MockClient((_) async => Response(await File(filePath).readAsString(), 200, headers: headers));
